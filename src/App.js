40a2683e9f5a362c5a78f21d112f7af4a0fe083d
Rasim <email@example.com>
2022-10-28 02:24:45 +0500

    app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import './App.css';
-// import {useEffect} from "react";
-// import {useTelegram} from "./hooks/useTelegram";
+import {useEffect} from "react";
+import {useTelegram} from "./hooks/useTelegram";
 // import Header from "./components/Header/Header";
 // import {Route, Routes} from 'react-router-dom'
 // import ProductList from "./components/ProductList/ProductList";
 // import Form from "./components/Form/Form";
 
+
 function App() {
-  // const {onToggleButton, tg} = useTelegram();
-  //
-  useEffect(() => {
-    tg.ready();
-  }, [])
 
-    const onClose = () => {
-      tg.close()
-    }
+    const {onToggleButton, tg} = useTelegram();
 
-  return (
+    useEffect(() => {
+        tg.ready();
+    }, [])
+
+
+    return (
       <div className="App">
           work
           <button onClick={onClose}>Закрыть</button>
