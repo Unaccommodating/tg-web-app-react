d93daac282fe008e40db4b0a2ce957b788e64699
Rasim <email@example.com>
2022-11-11 01:00:02 +0500

    check filter button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,20 +3,27 @@ import Button from "../Button/Button";
 import {useTelegram} from "../../hooks/useTelegram";
 import './Header.css';
 import Filter from "./Filter/Filter";
+import {useLocation} from "react-router-dom";
 
 const Header = () => {
     const [modalFilterActive, setFilterModalActive] = useState(false);
 
     const {onClose} = useTelegram();
 
+    function headerView() {
+        const location = useLocation();
+        return location.pathname;
+    }
 
     return (
         <div className={'header'}>
             <Button onClick={onClose}>Закрыть</Button>
-            <span className={'filter_btn'}>
-                <Button onClick={() => setFilterModalActive(true)}>Фильтр</Button>
-            </span>
-            <Filter active={modalFilterActive} setActive={setFilterModalActive}>Фильтр</Filter>
+            {(headerView() === '/') ?
+                <div className={'filter_btn'}>
+                    <Button onClick={() => setFilterModalActive(true)}>Фильтр</Button>
+                    <Filter active={modalFilterActive} setActive={setFilterModalActive}/>
+                </div> : null
+            }
         </div>
     );
 };
